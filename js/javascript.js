a56446b83c66d8a4bd1d8359be0d1edbf8649c74
ares vazquez bravo <ares@MacBook-Pro-de-ares.local>
2020-05-28 21:23:48 -0500

    logo responsive

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,4 @@ $(document).on('ready', function(){
 
 // ------------- PRELOADER
     TweenLite.to('.preloader', 1, {delay: 1.5, opacity: 0, display: 'none'});
-    TweenLite.to('.g-font-size-18--xs g-color--white ti-angle-double-down', 1, {repeat: 0, yoyo: true});
-
 });
